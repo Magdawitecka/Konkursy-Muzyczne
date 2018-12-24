96afad01ac92892c80068fd1384c43c99066190c
Magdawitecka <magda.witecka@wp.pl>
2018-12-24 23:37:47 +0100

    Core

---
+++ KonkursyMuzyczne/StronaGłówna.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace KonkursyMuzyczne
+{
+    partial class StronaGłówna
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otwórzDokumentacjęToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacjeOProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.edycjaToolStripMenuItem,
+            this.pomocToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(544, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zakończToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            this.zakończToolStripMenuItem.Click += new System.EventHandler(this.zakończToolStripMenuItem_Click);
+            // 
+            // edycjaToolStripMenuItem
+            // 
+            this.edycjaToolStripMenuItem.Name = "edycjaToolStripMenuItem";
+            this.edycjaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.edycjaToolStripMenuItem.Text = "Edycja";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otwórzDokumentacjęToolStripMenuItem,
+            this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.informacjeOProgramieToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem.Text = "Pomoc";
+            // 
+            // otwórzDokumentacjęToolStripMenuItem
+            // 
+            this.otwórzDokumentacjęToolStripMenuItem.Name = "otwórzDokumentacjęToolStripMenuItem";
+            this.otwórzDokumentacjęToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.otwórzDokumentacjęToolStripMenuItem.Text = "Otwórz dokumentację";
+            this.otwórzDokumentacjęToolStripMenuItem.Click += new System.EventHandler(this.otwórzDokumentacjęToolStripMenuItem_Click);
+            // 
+            // informacjeOProgramieToolStripMenuItem
+            // 
+            this.informacjeOProgramieToolStripMenuItem.Name = "informacjeOProgramieToolStripMenuItem";
+            this.informacjeOProgramieToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.informacjeOProgramieToolStripMenuItem.Text = "Informacje o programie";
+            this.informacjeOProgramieToolStripMenuItem.Click += new System.EventHandler(this.informacjeOProgramieToolStripMenuItem_Click);
+            // 
+            // otwórzPrzewodnikPoAplikacjiToolStripMenuItem
+            // 
+            this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem.Name = "otwórzPrzewodnikPoAplikacjiToolStripMenuItem";
+            this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem.Text = "Otwórz przewodnik po aplikacji";
+            this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem.Click += new System.EventHandler(this.otwórzPrzewodnikPoAplikacjiToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(236, 6);
+            // 
+            // StronaGłówna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 370);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "StronaGłówna";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edycjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacjeOProgramieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otwórzDokumentacjęToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otwórzPrzewodnikPoAplikacjiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ KonkursyMuzyczne/StronaGłówna.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonkursyMuzyczne
+{
+    public partial class StronaGłówna : Form
+    {
+        public StronaGłówna()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void zakończToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Deklarowanie treści komunikatu
+            string wiadomosc = "Czy jestes pewny ze chcesz zakonczyc program?";
+            string tytul = "Zamniecie";
+
+            //Komunikat do zamknięcia programu
+            var rezultat = MessageBox.Show(wiadomosc, tytul,
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            //Zamknięcie programu
+            if (rezultat == DialogResult.Yes)
+            {
+                Dispose();
+            }
+        }
+
+        private void informacjeOProgramieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Deklarowanie treści komunikatu
+            string wiadomosc = "Program przygotowany w ramach przedmiotu ADO.NET\nAplikacja ma za zadanie łączenie się z bazą danych uczestników konkursów muzycznych oraz możliwość jej edycji.\n\nPrzygotowali: Maciej Jabłoński, Magda Witecka\n\nProwadzący: dr Karol Korczak\n\nInformatyka i Ekonometria Rok III 2018-2019 Semestr zimowy";
+            string tytul = "O programie";
+
+            //Komunikat z informacjami o programie
+            MessageBox.Show(wiadomosc, tytul, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void otwórzDokumentacjęToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Otwieranie dokumentacji
+            System.Diagnostics.Process.Start(@"C:\Users\Magda1\Desktop\dokumentacja.odt");
+        }
+
+        private void otwórzPrzewodnikPoAplikacjiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Otwieranie przewodnika użytkownika
+            System.Diagnostics.Process.Start(@"C:\Users\Magda1\Desktop\dokumentacja.odt");
+        }
+    }
+}
